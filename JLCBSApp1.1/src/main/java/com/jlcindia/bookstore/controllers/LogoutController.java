36ceccf3bcffc067f42c952c9602e044f1102014
Karuna-Karri <karrikaruna20@gmail.com>
2024-12-21 17:45:42 +0530

    file

---
+++ JLCBSApp1.1/src/main/java/com/jlcindia/bookstore/controllers/LogoutController.java
@@ -0,0 +1,14 @@
+ package  com.jlcindia.bookstore.controllers; 
+import javax.servlet.http.*;
+public class LogoutController extends JLCBaseController{ 
+public String process(HttpServletRequest req,HttpServletResponse res) throws Exception{ 
+System.out.println("LogoutController-process()"); 
+String page="login.jsp"; 
+HttpSession session=req.getSession(false); 
+if(session!=null) 
+session.invalidate(); 
+String logoutMsg="You have logged out Successfully"; 
+req.setAttribute("LogoutMsg", logoutMsg); 
+return page; 
+} 
+} 
---
+++ JLCBSApp1.1/src/main/java/com/jlcindia/bookstore/dao/DAOFactory.java
@@ -0,0 +1,10 @@
+ package com.jlcindia.bookstore.dao;
+ public class DAOFactory { 
+	 static UserDAO userDAO; 
+	 static { 
+	 userDAO = new UserDAOImpl(); 
+	 } 
+	 public static UserDAO getUserDAO() { 
+	 return userDAO; 
+	 } 
+	}
